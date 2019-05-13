df3b0eae2bf98645d21301b61df2aaa5697f8f04
Chris Reddington <christian.reddington@outlook.com>
2019-05-13 23:21:46 +0100

    Adjust to handle auth for PostShow and not other functions

--- src/PostShow.cs
+++ src/PostShow.cs
@@ -5,6 +5,8 @@ using Newtonsoft.Json;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Threading;
 
 namespace Theatreers.Show
 {
@@ -16,26 +18,35 @@ namespace Theatreers.Show
             [OrchestrationClient] DurableOrchestrationClientBase starter,
             ILogger log)
         {
-            //Initialise the message for transport
-            //Generate correllation ID and initial request timestamp
-            string CorrelationId = Guid.NewGuid().ToString();
-            DecoratedShowMessage showObjectInput = await req.Content.ReadAsAsync<DecoratedShowMessage>();
-            MessageHeaders messageHeaders = new MessageHeaders();
-            messageHeaders.RequestCorrelationId = CorrelationId;
-            messageHeaders.RequestCreatedAt = DateTime.Now.ToString();
-            showObjectInput.MessageProperties = messageHeaders;
-            string eventData = JsonConvert.SerializeObject(showObjectInput);
+            if (Thread.CurrentPrincipal.Identity.IsAuthenticated)
+            {
+                //Initialise the message for transport
+                //Generate correllation ID and initial request timestamp
+                string CorrelationId = Guid.NewGuid().ToString();
+                DecoratedShowMessage showObjectInput = await req.Content.ReadAsAsync<DecoratedShowMessage>();
+                MessageHeaders messageHeaders = new MessageHeaders();
+                messageHeaders.RequestCorrelationId = CorrelationId;
+                messageHeaders.RequestCreatedAt = DateTime.Now.ToString();
+                showObjectInput.MessageProperties = messageHeaders;
+                string eventData = JsonConvert.SerializeObject(showObjectInput);
 
-            //Call the downstream "Activity" functions
-            string submitShowInstanceId = await starter.StartNewAsync("SubmitShowAsync", eventData);
-            string submitNewsInstanceId = await starter.StartNewAsync("SubmitNewsAsync", eventData);
-            string submitImageInstanceId = await starter.StartNewAsync("SubmitImageAsync", eventData);
+                //Call the downstream "Activity" functions
+                string submitShowInstanceId = await starter.StartNewAsync("SubmitShowAsync", eventData);
+                string submitNewsInstanceId = await starter.StartNewAsync("SubmitNewsAsync", eventData);
+                string submitImageInstanceId = await starter.StartNewAsync("SubmitImageAsync", eventData);
 
-            log.LogInformation($"[Request Correlation ID: {messageHeaders.RequestCorrelationId}] :: Begin Orchestration :: SubmitShowAsync instance ID: {submitImageInstanceId}");
-            log.LogInformation($"[Request Correlation ID: {messageHeaders.RequestCorrelationId}] :: Begin Orchestration :: SubmitNewsAsync instance ID: {submitNewsInstanceId}");
-            log.LogInformation($"[Request Correlation ID: {messageHeaders.RequestCorrelationId}] :: Begin Orchestration :: SubmitImageAsync instance ID: {submitImageInstanceId}");
+                log.LogInformation($"[Request Correlation ID: {messageHeaders.RequestCorrelationId}] :: Begin Orchestration :: SubmitShowAsync instance ID: {submitImageInstanceId}");
+                log.LogInformation($"[Request Correlation ID: {messageHeaders.RequestCorrelationId}] :: Begin Orchestration :: SubmitNewsAsync instance ID: {submitNewsInstanceId}");
+                log.LogInformation($"[Request Correlation ID: {messageHeaders.RequestCorrelationId}] :: Begin Orchestration :: SubmitImageAsync instance ID: {submitImageInstanceId}");
 
-            return starter.CreateCheckStatusResponse(req, submitShowInstanceId);
+                return starter.CreateCheckStatusResponse(req, submitShowInstanceId);
+            } else
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response.StatusCode = System.Net.HttpStatusCode.Forbidden;
+                response.ReasonPhrase = "The user is not logged in";
+                return response;
+            }
         }
     }
 }
