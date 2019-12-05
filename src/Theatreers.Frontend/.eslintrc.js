0f35b5026a7f7f27326c5133af4288f0ce72c568
Chris Reddington <christian.reddington@outlook.com>
2019-12-05 23:32:19 +0000

    temp turn console and debug on

--- src/Theatreers.Frontend/.eslintrc.js
+++ src/Theatreers.Frontend/.eslintrc.js
@@ -5,8 +5,10 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
+    "no-console": "on",
+    "no-debugger": "off"
     //"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    //"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
     parser: "babel-eslint"
