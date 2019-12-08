cc1834dd595f08de2418e9ec6446fcd0db9f0cee
Chris Reddington <christian.reddington@outlook.com>
2019-12-08 09:59:03 +0000

    Trigger pipeline

--- src/Theatreers.Show/Startup.cs
+++ src/Theatreers.Show/Startup.cs
@@ -22,6 +22,7 @@ namespace Theatreers.Show
 {
   public class Startup : FunctionsStartup
   {
+    //dummy comment
     private static string _databaseId = "theatreers";
     private static string _imageCollectionName = "shows";
     private static string _newsCollectionName = "shows";
