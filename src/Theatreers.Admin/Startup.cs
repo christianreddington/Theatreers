e9a67ca46398048fe0a897f4f0d6f47796cff14a
Chris Reddington <christian.reddington@outlook.com>
2019-12-18 19:42:46 +0000

    Add Swagger to Admin

---
+++ src/Theatreers.Admin/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using AzureFunctions.Extensions.Swashbuckle;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+
+namespace Theatreers.Admin
+{
+    class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            //Register the extension
+            builder.AddSwashBuckle(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
