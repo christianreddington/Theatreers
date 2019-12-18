de72c7658fac1ced8bc2f9c8904ebbf3d5a52b35
Chris Reddington <christian.reddington@outlook.com>
2019-12-18 21:23:23 +0000

    Adjust Auth level

--- src/Theatreers.Admin/Functions/Swagger.cs
+++ src/Theatreers.Admin/Functions/Swagger.cs
@@ -12,7 +12,7 @@ namespace Theatreers.Admin.Functions
         [SwaggerIgnore]
         [FunctionName("Swagger")]
         public static Task<HttpResponseMessage> Swagger(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "swagger/json")]
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "swagger/json")]
             HttpRequestMessage req,
             [SwashBuckleClient] ISwashBuckleClient swashBuckleClient)
         {
@@ -22,7 +22,7 @@ namespace Theatreers.Admin.Functions
         [SwaggerIgnore]
         [FunctionName("SwaggerUi")]
         public static Task<HttpResponseMessage> SwaggerUi(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "swagger/ui")]
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "swagger/ui")]
             HttpRequestMessage req,
             [SwashBuckleClient] ISwashBuckleClient swashBuckleClient)
         {
