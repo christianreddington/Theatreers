93b8b31e19e02d2bab6c5a3c2648053641ce3841
Chris Reddington <christian.reddington@outlook.com>
2019-12-18 18:36:05 +0000

    Add Swagger to Show API

--- src/Theatreers.Show/Startup.cs
+++ src/Theatreers.Show/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Theatreers.Core.Providers;
@@ -9,18 +10,21 @@ using Microsoft.Azure.Documents;
 using Theatreers.Show.Utils;
 using Theatreers.Show.Abstractions;
 using Theatreers.Show.Actions;
+using AzureFunctions.Extensions.Swashbuckle;
 using Theatreers.Show;
 using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Linq;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
 
 [assembly: FunctionsStartup(typeof(Theatreers.Show.Startup))]
 
 namespace Theatreers.Show
 {
-  public class Startup : FunctionsStartup
+  public class Startup : IWebJobsStartup
   {
     //Test
     private static string _databaseId = "theatreers";
@@ -32,11 +36,12 @@ namespace Theatreers.Show
     private static Uri _newsCollectionUri = UriFactory.CreateDocumentCollectionUri(_databaseId, _newsCollectionName);
     private static Uri _showCollectionUri = UriFactory.CreateDocumentCollectionUri(_databaseId, _showCollectionName);
     private static Uri _showlistCollectionUri = UriFactory.CreateDocumentCollectionUri(_databaseId, _showlistCollectionName);
-    public override void Configure(IFunctionsHostBuilder builder)
+    public void Configure(IWebJobsBuilder builder)
     {
       CosmosDBConnectionString cosmosDBConnectionString = new CosmosDBConnectionString(CosmosDBConnectionString.GetCosmosConnectionString("cosmosConnectionString"));
       IDocumentClient client = new DocumentClient(cosmosDBConnectionString.ServiceEndpoint, cosmosDBConnectionString.AuthKey);
-      builder.Services.AddScoped<IStorageProvider<ImageObject>, CosmosStorageProvider<ImageObject>>((s) => { return new CosmosStorageProvider<ImageObject>(client, _imageCollectionUri, _databaseId, _imageCollectionName); });
+      builder.AddSwashBuckle(Assembly.GetExecutingAssembly());
+            builder.Services.AddScoped<IStorageProvider<ImageObject>, CosmosStorageProvider<ImageObject>>((s) => { return new CosmosStorageProvider<ImageObject>(client, _imageCollectionUri, _databaseId, _imageCollectionName); });
       builder.Services.AddScoped<IStorageProvider<NewsObject>, CosmosStorageProvider<NewsObject>>((s) => { return new CosmosStorageProvider<NewsObject>(client, _newsCollectionUri, _databaseId, _newsCollectionName); });
       builder.Services.AddScoped<IStorageProvider<ShowObject>, CosmosStorageProvider<ShowObject>>((s) => { return new CosmosStorageProvider<ShowObject>(client, _showCollectionUri, _databaseId, _showCollectionName); });
       builder.Services.AddScoped<IStorageProvider<ShowListObject>, CosmosStorageProvider<ShowListObject>>((s) => { return new CosmosStorageProvider<ShowListObject>(client, _showlistCollectionUri, _databaseId, _showlistCollectionName); });
