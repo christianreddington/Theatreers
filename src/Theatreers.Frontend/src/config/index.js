0c58b4ba4108fd252f9e36022c168f9522b4326f
Chris Reddington <christian.reddington@outlook.com>
2019-12-29 13:35:51 +0000

    Fix redirect config for prod

--- src/Theatreers.Frontend/src/config/index.js
+++ src/Theatreers.Frontend/src/config/index.js
@@ -2,6 +2,6 @@ export default {
   clientId: "74568295-9e88-47db-bd81-e99a23fdcee8",
   authority: "https://theatreers.b2clogin.com/theatreers.onmicrosoft.com/B2C_1_SiUpIn",
   validateAuthority: false,
-  redirectUri: "http://localhost:8080"
-  //redirectUri: "https://www.theatreers.com"
+  //redirectUri: "http://localhost:8080"
+  redirectUri: "https://www.theatreers.com"
   }
